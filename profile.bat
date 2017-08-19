2834d2a0691007c98b7f1255136766dcae658a9a
aurhat <szyk@vivaldi.net>
2017-08-19 17:23:38 +0200

    Initial CSL version file

--- profile.bat
+++
@@ -1,9 +0,0 @@
-@SET PT="%PROGRAMFILES%\Microsoft Visual Studio 10.0\Team Tools\Performance Tools"
-
-%PT%\vsperfcmd /start:sample /output:profile\profile.vsp
-%PT%\vsperfcmd /launch:csl.exe
-%PT%\vsperfcmd /shutdown
-
-%PT%\vsperfreport /summary:all /output:profile\ profile\profile.vsp
-
-@PAUSE
